6a7b6ab610489d08134d9444c0dab59fc02577c0
Islamova Akmaral <akmaralslmv@gmail.com>
2020-03-02 11:18:31 +0600

    Updates

---
+++ precache-manifest.d24027f1f48e045c74d517132888296c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6656f74324f1f3d8202680ec08afe5e8",
+    "url": "/reactive-pomodoro-app/index.html"
+  },
+  {
+    "revision": "eef8b5774bcae826a349",
+    "url": "/reactive-pomodoro-app/static/css/main.69a265e1.chunk.css"
+  },
+  {
+    "revision": "80e8c12de3d07d210769",
+    "url": "/reactive-pomodoro-app/static/js/2.25a93a81.chunk.js"
+  },
+  {
+    "revision": "eef8b5774bcae826a349",
+    "url": "/reactive-pomodoro-app/static/js/main.a47a8c70.chunk.js"
+  },
+  {
+    "revision": "1079d993c4dceca3e3ad",
+    "url": "/reactive-pomodoro-app/static/js/runtime-main.b08707d7.js"
+  },
+  {
+    "revision": "691d4f019779152bbcce182e658265c5",
+    "url": "/reactive-pomodoro-app/static/media/settings.691d4f01.svg"
+  }
+]);
